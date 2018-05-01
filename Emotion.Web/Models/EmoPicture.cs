f519185ef2920ae43d95913d25bed9904430df45
sbpinilla <sbpinilla@hotmail.com>
2018-05-01 12:14:29 -0500

    Primer commit

---
+++ Emotion.Web/Models/EmoPicture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace Emotion.Web.Models
+{
+    public class EmoPicture
+    {
+        public int Id { get; set; }
+        public String Name{ get; set; }
+        public String  Path  { get; set; }
+
+        public virtual ObservableCollection<EmoFace> Faces { get; set; }
+
+    }
+}
